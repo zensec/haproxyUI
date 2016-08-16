42a9d0077a5fea6dc6046626a99f05d073ea1c26
Dan Longshaw <dan@exile-tek.com>
2016-08-16 03:00:40 +0100

    Adds active_cluster count to dashboard

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -2,13 +2,15 @@ from flask import render_template
 from .base import BaseView
 from app.models.server import Server
 from app.models.domain import Domain
+from app.models.cluster import Cluster
 
 
 class Dashboard(BaseView):
     def index(self):
         stats = {
             'online_servers': Server.query.filter_by(audit_is_deleted=False, is_active=True).count(),
-            'active_domains': Domain.query.filter_by(audit_is_deleted=False, is_active=True).count()
+            'active_domains': Domain.query.filter_by(audit_is_deleted=False, is_active=True).count(),
+            'active_clusters': Cluster.query.filter_by(audit_is_deleted=False, is_active=True).count()
         }
         return render_template('dashboard/index.html',
                                page_title='Dashboard',
