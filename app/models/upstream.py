ee7e55a44a0413d966f04c75adee6675357edcb3
Dan Longshaw <dan@exile-tek.com>
2016-08-25 15:18:50 +0100

    Adds upstream model

---
+++ app/models/upstream.py
@@ -0,0 +1,17 @@
+from .base import Audit
+from app.helpers import parse_to_dict
+from app import db
+
+
+class Upstream(Audit):
+    __tablename__ = 'upstreams'
+    id = db.Column(db.Integer, primary_key=True)
+    domain_id = db.Column(db.Integer, db.ForeignKey('domains.id'))
+    ip_address = db.Column(db.String(100))
+    port = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return 'Upstream {0} for domain {1]>'.format(self.ip_address, self.domain.name)
+
+    def to_dict(self):
+        return parse_to_dict(self)
