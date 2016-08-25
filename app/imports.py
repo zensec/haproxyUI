4f4366a5f4199f3dcd2b9937188a7fb8e4de340d
Dan Longshaw <dan@exile-tek.com>
2016-08-25 14:18:05 +0100

    Imports new blueprints

--- app/imports.py
+++ app/imports.py
@@ -5,10 +5,14 @@ def register_blueprints(flask_app):
     from app.views.clusters import Clusters
     Clusters.register(flask_app)
 
+    from app.views.servers import Servers
+    Servers.register(flask_app)
+
     from app.views.dashboard import Dashboard
     Dashboard.register(flask_app)
 
     from app.models.server import Server
     from app.models.domain import Domain
+    from app.models.log import Log
 
     return
