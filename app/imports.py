9e8735f5f1cad4862c261c02942a82e07ed96169
Dan Longshaw <dan@exile-tek.com>
2016-08-25 14:53:34 +0100

    Adds domains blueprint to imports

--- app/imports.py
+++ app/imports.py
@@ -11,6 +11,9 @@ def register_blueprints(flask_app):
     from app.views.dashboard import Dashboard
     Dashboard.register(flask_app)
 
+    from app.views.domains import Domains
+    Domains.register(flask_app)
+
     from app.models.server import Server
     from app.models.domain import Domain
     from app.models.log import Log
